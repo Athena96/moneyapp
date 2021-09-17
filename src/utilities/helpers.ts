abc60682642ec77c91d0595049fc9ca7de791284
Jared Franzone <jffranzo@amazon.com>
2021-09-17 09:43:24 -0700

    forgot the helpers

--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -4,14 +4,18 @@
 
 
 export function dateRange(startDate: Date, endDate: Date, steps = 30) {
-    const dateArray = [];
-    let currentDate = new Date(startDate);
+  const dateArray = [];
+  let currentDate = new Date(startDate);
 
-    while (currentDate <= new Date(endDate)) {
-      dateArray.push(new Date(currentDate));
-      // Use UTC date to prevent problems with time zones and DST
-      currentDate.setUTCDate(currentDate.getUTCDate() + steps);
-    }
-
-    return dateArray;
+  while (currentDate <= new Date(endDate)) {
+    dateArray.push(new Date(currentDate));
+    // Use UTC date to prevent problems with time zones and DST
+    currentDate.setUTCDate(currentDate.getUTCDate() + steps);
   }
+
+  return dateArray;
+}
+
+export function getDay(date: Date) {
+
+}
