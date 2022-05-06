ddb637749eaf163c6466672d1bf309e98004ba1e
Jared Franzone <jaredfranzone@gmail.com>
2022-05-05 21:28:48 -0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,15 @@
 import * as React from 'react';
+import './App.css';
 
-// import React from "react";
 import { Amplify } from "aws-amplify";
 import {
   AmplifyProvider,
   Authenticator,
-  Flex,
-  Image,
-  Text,
-  View,
 } from "@aws-amplify/ui-react";
-
 import "@aws-amplify/ui-react/styles.css";
 import theme from "./theme";
-import logo from "./logo.svg";
-
-
-
-
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-
-import { Link } from "react-router-dom";
-
-import './App.css';
-import { Auth } from 'aws-amplify';
-
 import Main from './views/Main'
-import { moneyGreen } from './utilities/constants';
 import aws_exports from "./aws-exports";
 
 Amplify.configure(aws_exports);
@@ -65,43 +43,19 @@ class App extends React.Component<IProps, IState> {
 
     return (
       <div>
-{/*       
-        <div style={{textAlign: 'left'}}>
+        <div style={{ textAlign: 'left' }}>
 
-          <Typography variant="h2" component="div" gutterBottom>
-            Money<br/>Tomorrow
-          </Typography>
-        </div> */}
-        <div style={{textAlign: 'left'}}>
-
-        <AmplifyProvider theme={theme}>
-          <Authenticator >
-            {({ signOut, user }) => (
-              <>
-
-           
-                <Box sx={{ flexGrow: 1 }}>
-                  <AppBar sx={{ bgcolor: moneyGreen }} position="static">
-                    <Toolbar>
-
-                      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                      <Link style={{ color: 'white', textDecoration: 'none' }} to="/">Money Tomorrow {<small>{user && user.attributes && user.attributes.email ? user.attributes.email : ''}</small>}</Link>
-                      </Typography>
-                      <Button variant="outlined" style={{ color: 'white'}} onClick={signOut}>
-                        Sign Out
-                      </Button>
-                    </Toolbar>
-                  </AppBar>
-                </Box>
-
-                <Container >
-                  <Main />
-                </Container>
-              </>
-
-            )}
-          </Authenticator>
-        </AmplifyProvider>
+          <AmplifyProvider theme={theme}>
+            <Authenticator >
+              {({ signOut, user }) => (
+                <>
+                  <Container >
+                    <Main />
+                  </Container>
+                </>
+              )}
+            </Authenticator>
+          </AmplifyProvider>
         </div>
 
       </div>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import '../App.css';
+import { styled, Theme, CSSObject } from '@mui/material/styles';
+
 import BudgetsView from '../views/BudgetsView';
 import DataView from '../views/DataView';
 import EventsView from '../views/EventsView';
@@ -14,12 +16,84 @@ import Amplify from 'aws-amplify'
 import awsExports from "../aws-exports";
 import { Simulation } from '../model/Base/Simulation';
 import { SimulationDataAccess } from '../utilities/SimulationDataAccess';
+import MuiDrawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from "react-router-dom";
+import { moneyGreen } from '../utilities/constants';
+import CssBaseline from '@mui/material/CssBaseline';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
 
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
   value: number;
 }
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+// interface AppBarProps extends MuiAppBarProps {
+//   open?: boolean;
+// }
+
+// const AppBar = styled(MuiAppBar, {
+//   shouldForwardProp: (prop) => prop !== 'open',
+// })<AppBarProps>(({ theme, open }) => ({
+//   transition: theme.transitions.create(['margin', 'width'], {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.leavingScreen,
+//   }),
+//   ...(open && {
+//     width: `calc(100% - ${drawerWidth}px)`,
+//     marginLeft: `${drawerWidth}px`,
+//     transition: theme.transitions.create(['margin', 'width'], {
+//       easing: theme.transitions.easing.easeOut,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//   }),
+// }));
 
 function TabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props;
@@ -41,6 +115,14 @@ function TabPanel(props: TabPanelProps) {
     </div>
   );
 }
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
 
 function a11yProps(index: number) {
   return {
@@ -58,6 +140,7 @@ interface IState {
   selectedTab: number;
   user: string | undefined;
   simulation: Simulation | undefined;
+  open: boolean;
 }
 
 class Home extends React.Component<IProps, IState> {
@@ -65,14 +148,17 @@ class Home extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.state = {
-      selectedTab: 1,
+      selectedTab: 0,
       user: undefined,
-      simulation: undefined
+      simulation: undefined,
+      open: false
     }
 
     this.render = this.render.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.newTab = this.newTab.bind(this);
+    this.handleDrawerOpen = this.handleDrawerOpen.bind(this);
+    this.handleSignOut = this.handleSignOut.bind(this);
   }
 
   async componentDidMount() {
@@ -82,7 +168,12 @@ class Home extends React.Component<IProps, IState> {
 
     this.setState({ user: email, simulation: userSim });
   }
+  handleDrawerOpen() {
+    console.log('handleDrawerOpen')
+    const currVal = this.state.open;
+    this.setState({ open: !currVal });
 
+  }
   handleChange(event: React.SyntheticEvent, newValue: number) {
     this.setState({ selectedTab: newValue });
   }
@@ -90,6 +181,13 @@ class Home extends React.Component<IProps, IState> {
     this.setState({ selectedTab: newValue });
   }
 
+  async handleSignOut() {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+  }
 
   render() {
 
@@ -97,58 +195,91 @@ class Home extends React.Component<IProps, IState> {
     if (this.state.user) {
       return (
 
-        <Box sx={{ flexGrow: 1, display: 'flex' }}>
-          <Tabs
-            orientation="vertical"
-            variant="scrollable"
+        <Box sx={{ display: 'flex' }}>
 
-            value={this.state.selectedTab}
-            onChange={this.handleChange}
-            aria-label="Vertical tabs example"
-            sx={{ borderRight: `1px`, borderColor: 'divider', overflow: `visible`, }}
+
+
+          <AppBar position="fixed" sx={{ bgcolor: moneyGreen }} >
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="default"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+                onClick={this.handleDrawerOpen}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Link style={{ color: 'white', textDecoration: 'none' }} to="/">Money Tomorrow {<small>{this.state.user}</small>}</Link>
+              </Typography>
+
+              <Button variant="outlined" style={{ color: 'white' }} onClick={this.handleSignOut}>
+                Sign Out
+              </Button>
+
+            </Toolbar>
+
+          </AppBar>
+          <MuiDrawer
+            anchor="left"
+            open={this.state.open}
           >
-            {/* <Tab label="Accounts" /> */}
+            <DrawerHeader>
+              <IconButton onClick={this.handleDrawerOpen}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </DrawerHeader>
 
-            <Tab label="Graph" {...a11yProps(0)} />
-            <Tab label="Budgets" {...a11yProps(1)} />
-            <Tab label="Events" {...a11yProps(2)} />
-            <Tab label="Assets" {...a11yProps(3)} />
-            <Tab label="Data" {...a11yProps(4)} />
-            <Tab label="Simulations" {...a11yProps(4)} />
+            <Divider />
+            <Tabs
+              orientation="vertical"
+              variant="scrollable"
 
+              value={this.state.selectedTab}
+              onChange={this.handleChange}
+              aria-label="Vertical tabs example"
+              sx={{ borderRight: `1px`, borderColor: 'divider', overflow: `visible`, }}
+            >
+              <Tab label="Graph" {...a11yProps(0)} />
+              <Tab label="Budgets" {...a11yProps(1)} />
+              <Tab label="Events" {...a11yProps(2)} />
+              <Tab label="Assets" {...a11yProps(3)} />
+              <Tab label="Data" {...a11yProps(4)} />
+              <Tab label="Simulations" {...a11yProps(4)} />
+            </Tabs>
 
-          </Tabs>
+          </MuiDrawer>
 
-          <TabPanel value={this.state.selectedTab} index={0} >
-            <GraphView user={this.state.user} simulation={this.state.simulation} change={this.newTab} />
-          </TabPanel>
+          <Container >
+            <DrawerHeader />
 
+            <TabPanel value={this.state.selectedTab} index={0} >
+              <GraphView user={this.state.user} simulation={this.state.simulation} change={this.newTab} />
+            </TabPanel>
 
-          <TabPanel value={this.state.selectedTab} index={1}>
-            <BudgetsView user={this.state.user} simulation={this.state.simulation} change={this.newTab} />
-          </TabPanel>
+            <TabPanel value={this.state.selectedTab} index={1}>
+              <BudgetsView user={this.state.user} simulation={this.state.simulation} change={this.newTab} />
+            </TabPanel>
 
-          <TabPanel value={this.state.selectedTab} index={2}>
-            <EventsView user={this.state.user} simulation={this.state.simulation} change={this.newTab} />
-          </TabPanel>
+            <TabPanel value={this.state.selectedTab} index={2}>
+              <EventsView user={this.state.user} simulation={this.state.simulation} change={this.newTab} />
+            </TabPanel>
 
-          <TabPanel value={this.state.selectedTab} index={3}>
-            <AssetsView user={this.state.user} simulation={this.state.simulation} change={this.newTab} />
-          </TabPanel>
+            <TabPanel value={this.state.selectedTab} index={3}>
+              <AssetsView user={this.state.user} simulation={this.state.simulation} change={this.newTab} />
+            </TabPanel>
 
-          <TabPanel value={this.state.selectedTab} index={4}>
-            <DataView user={this.state.user} simulation={this.state.simulation} change={this.newTab} />
-          </TabPanel>
-       
-          <TabPanel value={this.state.selectedTab} index={5}>
-            <SimulationView user={this.state.user} simulation={this.state.simulation} />
-          </TabPanel>
+            <TabPanel value={this.state.selectedTab} index={4}>
+              <DataView user={this.state.user} simulation={this.state.simulation} change={this.newTab} />
+            </TabPanel>
 
- 
+            <TabPanel value={this.state.selectedTab} index={5}>
+              <SimulationView user={this.state.user} simulation={this.state.simulation} />
+            </TabPanel>
+          </Container>
 
         </Box>
-
-
       );
     } else {
       return (<></>)
