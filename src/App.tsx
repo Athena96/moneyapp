840abff80a880fe9fbddd453371aea41f1895c02
Jared Franzone <jaredfranzone@gmail.com>
2022-07-26 20:19:38 -0700

    improve sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,12 @@ import {
 import "@aws-amplify/ui-react/styles.css";
 import theme from "./theme";
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
 import aws_exports from "./aws-exports";
 import Home from './views/Home';
 import { Auth } from 'aws-amplify';
 import SignInView from './views/SignInView';
+import { moneyGreen } from './utilities/constants';
 
 Amplify.configure(aws_exports);
 
@@ -68,8 +70,12 @@ class App extends React.Component<IProps, IState> {
     // have a did setup in Inputs.... if hasDoneSetup, load Home, otherwise load Setup
     return (
       <div>
-        <div style={{ textAlign: 'left' }}>
-          {!this.state.signedIn && <SignInView />}
+        <div >
+  
+        <Stack direction='row' spacing={2}>
+    
+          <div style={{marginTop: '175px', marginLeft: '30px'}}>
+            <p style={{color: moneyGreen, textAlign: 'center'}}><b>Sign in to your Account</b></p>
           <AmplifyProvider theme={theme}>
             <Authenticator >
               {({ signOut, user }) => {
@@ -83,6 +89,14 @@ class App extends React.Component<IProps, IState> {
               }
             </Authenticator>
           </AmplifyProvider>
+          </div>
+
+          <div >
+          {!this.state.signedIn && <SignInView />}
+
+          </div>
+         </Stack>
+          
         </div>
       </div>
     );
--- src/views/SignInView.tsx
+++ src/views/SignInView.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { moneyGreen } from '../utilities/constants';
+import main from '../images/main.png'
+import Paper from '@mui/material/Paper';
 
 interface SignInViewProps {
 
@@ -21,9 +23,29 @@ class SignInView extends React.Component<SignInViewProps, IState> {
 
     render() {
         return (
-            <div style={{ textAlign: "center" }}>
-                <h1 style={{ color: moneyGreen }}>Money Tomorrow</h1>
-                <h3 style={{ color: "grey" }}>Money can't buy happiness, but it can buy... peace 🧘</h3>
+            <div style={{ textAlign: "left", margin: '20px' }}>
+        <h1 style={{ color: moneyGreen }}>Money Tomorrow</h1>
+        <h2 style={{ color: "grey" }}>Money can't buy happiness, but it can buy... peace 🧘</h2>
+                
+                <hr/>
+
+                <h3 style={{ color: "grey" }}>Retire with confidence.</h3>
+
+                <p >This is a simple, free tool that uses Monte Carlo simulation to help you determine when you can retire.</p>
+                <p >You input how much you're saving, your living expenses, and your assets and we uses this to project out how your portfolio will perform!</p>
+
+                <p >Create your free account today to answer the question of when you can retire, if might be sooner than you think!</p>
+
+
+                {/* <Card variant="outlined"> */}
+                <Paper elevation={3} style={{borderRadius: '10px'}} >
+                <img src={main} alt="main"  style={{borderRadius: '10px', width: '100%'}}  />
+
+                    </Paper>
+
+                {/* </Card> */}
+
+
                 <br />
             </div>
         );
